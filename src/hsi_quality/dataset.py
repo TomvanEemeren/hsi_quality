c80265c69a80784af7b2a847bc174725766dc50e
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2026-04-29 13:23:28 +0200

    Add dataset class

---
+++ src/hsi_quality/dataset.py
@@ -0,0 +1,67 @@
+import xarray as xr
+import pandas as pd
+import datetime as dt
+
+from hypso import Hypso2
+
+class Dataset:
+    def __init__(self, dataframe: pd.DataFrame):
+        self.df = dataframe
+
+    def get_capture(self, target: str, timestamp: str):
+        # Create the path to the netcdf file
+        file_name = f"{target}_{timestamp}-l1d.nc"
+        path = f"datasets/{target}/processed/{file_name}"
+
+        # Load the capture
+        satobj = self._load_nc_file(path)
+
+        return satobj
+
+    def filter(self, func):
+        mask = self.df.apply(func, axis=1)
+        filtered_df = self.df.loc[mask]
+        return Dataset(filtered_df)
+
+    def sort(self, by: str):
+        sorted_df = self.df.sort_values(by=by)
+        return Dataset(sorted_df)
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+
+        # Get metadata for the capture
+        target = row["location_description"]
+        timestamp = row["timestamp_acquired_string"]
+        timestamp = self._convert_timestamp(timestamp)
+
+        # Create the path to the netcdf file
+        file_name = f"{target}_{timestamp}-l1d.nc"
+        path = f"datasets/{target}/processed/{file_name}"
+
+        # Load the capture
+        satobj = self._load_nc_file(path)
+
+        return satobj
+
+    def _load_nc_file(self, path: str):
+
+        # Load the data and store it in a Hypso2 object
+        satobj = Hypso2(path=path, verbose=False)
+
+        return satobj
+    
+    def _convert_timestamp(self, timestamp: str):
+        format = "%Y-%m-%dT%H-%M-%SZ"
+
+        # Convert the timestamp string to a datetime object
+        timestamp = pd.to_datetime(timestamp, utc=True)
+        timestamp = timestamp.strftime(format)
+
+        return timestamp
+    
+
+
--- src/hsi_quality/visualize.py
+++ src/hsi_quality/visualize.py
@@ -1,29 +1,29 @@
 import os
-from pathlib import Path
 import numpy as np
 import xarray as xr
+from pathlib import Path
 from matplotlib import pyplot as plt
 
 from hypso import Hypso2
 from hypso.spectral_analysis import get_closest_wavelength_index
-from hypso.geometry.nearest import get_nearest_pixel
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = os.path.join(ROOT_DIR, "datasets")
 
-def plot_rgb(satobj_h2: Hypso2, cube: xr.DataArray, save: bool = False):
+def plot_rgb(satobj_h2: Hypso2, save: bool = False, verbose: bool = False) -> np.ndarray:
     """
     Visualize the hyperspectral datacube as an RGB image using the RGB wavelengths.
     The hyperspectral image has an aspect ratio of 1:8.
 
     Args:
         satobj_h2 (Hypso2): The Hypso2 satellite object.
-        cube (xr.DataArray): The hyperspectral datacube.
         save (bool): Whether to save the RGB image. Defaults to False.
-
+        verbose (bool): Whether to print verbose output. Defaults to False.
     Returns:
         np.ndarray: The RGB image as a NumPy array.
     """      
+    cube = satobj_h2.l1d_cube
+
     # Get wavelengths of capture
     satobj_h2.wavelengths
 
@@ -52,25 +52,29 @@ def plot_rgb(satobj_h2: Hypso2, cube: xr.DataArray, save: bool = False):
     p98 = np.percentile(rgb, 98, axis=(0, 1))
     rgb_norm = np.clip((rgb - p2) / (p98 - p2 + 1e-8), 0, 1)
 
-    # Flip and rotate the image
-    flipped_rgb = np.fliplr(rgb_norm)
-    rotated_rgb = np.rot90(flipped_rgb, k=1)
+    # Rotate the image for better visualization
+    rotated_rgb = np.rot90(rgb_norm, k=1)
 
     # Plot the RGB image
     fig, ax = plt.subplots()
     ax.imshow(rotated_rgb, aspect=1/8)
     ax.axis("off")
     
-    # Save the RGB image
     if save:
         name = satobj_h2.l1d_name
         target = name.split("_")[0]
-        os.makedirs(os.path.join(DATA_DIR, target, "reflectance"), exist_ok=True)
-        output_path = os.path.join(DATA_DIR, target, "reflectance", name + ".png")
+        os.makedirs(os.path.join(DATA_DIR, target, "images"), exist_ok=True)
+        output_path = os.path.join(DATA_DIR, target, "images", name + ".png")
 
+        # Save the RGB image
         fig.savefig(output_path, bbox_inches="tight", pad_inches=0, dpi=300)
-        print(f"Saved RGB image to {output_path}")
+        if verbose:
+            print(f"Saved RGB image to {output_path}")
     else:
+        if verbose:
+            name = satobj_h2.l1d_name
+            print(f"RGB image for {name}")
+
         # Display the RGB image
         plt.show()
     
