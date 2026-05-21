2bcbec15e72a6ff85fadc4f20548e29a219c6657
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2026-05-21 13:51:06 +0200

    Fix refactoring changes

--- analyze_data.py
+++ analyze_data.py
@@ -9,9 +9,9 @@ path = os.path.abspath(os.path.join(os.getcwd(),"src","hypso"))
 sys.path.append(path)
 
 from hsi_quality.data import Dataset, Storage
-from hsi_quality.analysis import Resampler
+from hsi_quality.analysis import Resampler, EdgeDetector, calculate_scores
 from hsi_quality.plotting import select_box, intersect_captures
-from hsi_quality.plotting import plot_metric, plot_resampled_images, plot_full_images, plot_cloud_images
+from hsi_quality.plotting import plot_resampled_images, plot_full_images, plot_cloud_images, plot_metric
 from hsi_quality.metrics import MeanSSIM, MvSSIM, QLambda, GRD
 from hsi_quality.utils import load_parameters
 
@@ -36,16 +36,28 @@ def main():
     # Initialize the resampler for the given roi
     resampler = Resampler(bbox=area_extent, zone=args.zone)
 
+    # Initialize the edge detector
+    cfg = load_parameters("edge_params")
+    ed = EdgeDetector(params=cfg)
+
     # Save visualizations of the selected area
     plot_resampled_images(dataset, resampler, save=True)
     plot_cloud_images(dataset, resampler, save=True)
 
     # Plot and save the metric as a function of off-nadir angle
     cfg = load_parameters("metric_params")
-    plot_metric(dataset, MvSSIM(params=cfg["MvSSIM"]), resampler, save=True)
-    plot_metric(dataset, MeanSSIM(params=cfg["MeanSSIM"]), resampler, save=True)
-    plot_metric(dataset, QLambda(params=cfg["Qlambda"]), resampler, save=True)
-    plot_metric(dataset, GRD(params=cfg["GRD"]), resampler, save=True)
+
+    scores = calculate_scores(dataset, MvSSIM(params=cfg["MvSSIM"]), resampler=resampler, save=True)
+    plot_metric(scores, save=True)
+
+    scores = calculate_scores(dataset, MeanSSIM(params=cfg["MeanSSIM"]), resampler=resampler, save=True)
+    plot_metric(scores, save=True)
+
+    scores = calculate_scores(dataset, QLambda(params=cfg["Qlambda"]), resampler=resampler, save=True)
+    plot_metric(scores, save=True)
+
+    scores = calculate_scores(dataset, GRD(params=cfg["GRD"]), ed=ed, save=True)
+    plot_metric(scores, save=True)
 
 if __name__ == "__main__":
     main()
